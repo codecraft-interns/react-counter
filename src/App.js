b40263bdbb3e96d6f7161c2840f18d4cd70fd244
tejasra <56512902+tejasra@users.noreply.github.com>
2020-04-23 17:21:58 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Design from './components/design.js'
+
+function App() {
+  return (
+    <div className="App">
+    <Design/> 
+    </div>
+  );
+}
+
+export default App;
