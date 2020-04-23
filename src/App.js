6525aadfdc117029fcc7c019a1c191141ef6bb59
Mohinuddin153 <mohinuddinmoinu123@gmail.com>
2020-04-23 20:36:25 +0530

    counter added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { render } from "@testing-library/react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <button class="button button-increment" onClick={this.decrement}>-</button>
+        <div className="count">{this.state.count}</div>
+        <button class="button button-decrement" onClick={this.increment}>+</button>
+      </div>
+    );
+  }
 }
 
 export default App;
