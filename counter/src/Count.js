48e52acd1f6c436c1302b70e99e78ff5fd68b596
MohammadAshfan12 <mohammadashfan48@gmail.com>
2020-04-23 18:59:48 -0700

    react counter

---
+++ counter/src/Count.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+class Count extends Component {
+    count = 0
+    constructor() {
+        super()
+        this.state = {
+            number: this.count
+        }
+    }
+
+    increment() {
+        this.setState({
+            number: ++(this.count)
+        })
+    }
+    
+    decrement() {
+        if(this.count > 0){
+        this.setState({
+            number: --(this.count)
+        })
+    }
+    }
+
+    render() {
+        return (
+            <div class="container"> 
+                <button className="btn-decrement" onClick={() => this.decrement()}><b>-</b></button>
+                <div class="number" id='number'>{this.state.number}</div>
+                <button className="btn-decrement btn-decrement--increment " onClick={() => this.increment()}><b>+</b></button>
+            </div>
+        )
+    }
+}
+
+export default Count
